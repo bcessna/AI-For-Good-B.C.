2a9fe961368e424e4debbd0f66742ba6077f1804
bcessna <166463883+bcessna@users.noreply.github.com>
2024-05-27 17:40:53 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -2,15 +2,11 @@ def test_load():
   return 'loaded'
 
 
-def cond_prob(table, A, ax, B, bx):
-  subtable = up_table_subset(table, A, 'equals', ax)
-  sublist = up_get_column(subtable, B)
-  ColumnA = up_get_column(table, A)
-  ColumnB = up_get_column(table, B)
-  pBA = sum([1 if i == bx else 0 for i in sublist])/len(sublist)
-  pA = sum([1 if i == ax else 0 for i in columnA])/len(ColumnA)
-  pA = sum([1 if i == bx else 0 for i in columnB])/len(ColumnB)
-  return pBA * pA/pB
+def cond_prob(table, evidence, evidence_value, target, target_value):
+  t_subset = up_table_subset(table, target, 'equals', target_value)
+  e_list = up_get_column(t_subset, evidence)
+  p_b_a = sum([1 if v==evidence_value else 0 for v in e_list])/len(e_list)
+  return p_b_a + .01  #Laplace smoothing factor
 
 def compute_probs(neg, pos): 
   p0 = neg/ (neg+pos)

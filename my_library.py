d6994e6fb664700cffb1e0992157cad61e2b9669
bcessna <166463883+bcessna@users.noreply.github.com>
2024-05-27 18:21:21 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -26,8 +26,8 @@ def naive_bayes(table, evidence_row, target):
   cond_prob_Y = cond_probs_product(table, evidence_row, target, 1)
   prior_prob_Y = prior_prob(table, target, 1)
 
-  prob_target_N = cond_prob_N * prior_prob_N 
-  prob_target_Y = cond_prob_Y * prior_prob_Y 
+  prob_target_N = (cond_prob_N)(prior_prob_N) 
+  prob_target_Y = (cond_prob_Y)(prior_prob_Y) 
 
   neg, pos = compute_probs(prob_target_N, prob_target_Y)
   return [neg, pos]
